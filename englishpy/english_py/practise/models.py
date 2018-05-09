497fb1237eda520e03f5e1ac24e1993150c3e3df
- Edwin Mendez - <emendez@unac.edu.co>
2018-05-09 11:45:01 -0500

    user foreignkey on routine

--- englishpy/english_py/practise/models.py
+++ englishpy/english_py/practise/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
@@ -60,9 +61,9 @@ class PastParticiple(models.Model):
         return data
 
 
-
 class Routine(models.Model):
     name = models.CharField(_('Nombre de la rutina'), max_length=255, unique=True)
+    user = models.ForeignKey(User, verbose_name=_('Usuario'), null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -71,6 +72,7 @@ class Routine(models.Model):
 class Topic(models.Model):
     name = models.CharField(_('Tema'), max_length=512, choices=TopicTypes.TYPES, default=TopicTypes.DEFAULT, unique=True)
     routine = models.ForeignKey(Routine, verbose_name=_('Rutina'))
+    learned_word = models.CharField(_('Palabra aprendida'), max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
