3a2b2a160eb684cca634fa2cc6c9d20c66bc60bd
- Edwin Mendez - <emendez@unac.edu.co>
2018-05-10 12:17:07 -0500

    repetitions field

--- englishpy/english_py/practise/models.py
+++ englishpy/english_py/practise/models.py
@@ -72,6 +72,7 @@ class Routine(models.Model):
 class Topic(models.Model):
     name = models.CharField(_('Tema'), max_length=512, choices=TopicTypes.TYPES, default=TopicTypes.DEFAULT, unique=True)
     routine = models.ForeignKey(Routine, verbose_name=_('Rutina'))
+    repetitions = models.IntegerField(_('Repeticiones'), default=5)
 
     def __str__(self):
         return self.name
--- englishpy/english_py/practise/review/views.py
+++ englishpy/english_py/practise/review/views.py
@@ -13,8 +13,9 @@ from django.conf import settings
 
 def home(request, template_name='practise/review/home.html'):
     data={}
-    data['verb'] = Present.objects.filter(~Q(verb__in=LearnedWord.objects.filter(topic__routine__user=request.user)
-                                             .values_list('word', flat=True))).first()
+    data['verb'] = Present.objects.filter(
+        ~Q(verb__in=LearnedWord.objects.filter(topic__routine__user=request.user)
+        .values_list('word', flat=True))).first()
 
     data['form'] = VerbForm()
     data['audio'] = 'media/english/verb/present/work.mp3'
@@ -22,25 +23,24 @@ def home(request, template_name='practise/review/home.html'):
 
 
 def next_verb(request):
-    data = {}
-
-    verb = Present.objects.filter(~Q(verb__in=LearnedWord.objects.filter(topic__routine__user=request.user)
-                                     .values_list('word', flat=True))).first()
+    verb = Present.objects.filter(~Q(verb__in=LearnedWord.objects.filter(
+        topic__routine__user=request.user)
+        .values_list('word', flat=True))
+        ).first()
 
     data = {
         'present': verb.present_object(),
         'past': verb.past(),
         'past_participle':verb.past_participle()
     }
-
-    print(data)
-
     return JsonResponse(data)
 
 
 @csrf_exempt
 def register_learned_word(request):
     data = {}
-    LearnedWord.objects.create(word=request.POST['learned_word'],
-                               topic=Topic.objects.get(routine__user=request.user, name=TopicTypes.VERBS))
+    if request.user.is_authenticated():
+        LearnedWord.objects.create(
+            word=request.POST['learned_word'],
+            topic=Topic.objects.get(routine__user=request.user, name=TopicTypes.VERBS))
     return JsonResponse(data)
