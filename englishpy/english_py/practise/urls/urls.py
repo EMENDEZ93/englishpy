fe79276907231266d2d596fae646884b4edbaef2
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-13 11:29:07 -0500

    setting urls

--- englishpy/english_py/practise/urls/urls.py
+++ englishpy/english_py/practise/urls/urls.py
@@ -1,6 +1,23 @@
 from django.conf.urls import url, include
+from .review.phrasal_verb.review_phrasal_views_urls import urlpatterns as review_phrasal_views_urls
+from .review.review_phrasal_views_urls import urlpatterns as review__review_phrasal_views_urls
+from .review.review_views_urls import urlpatterns as review_views_urls
+from .review.views_urls import urlpatterns as review__views_urls
+
+from .reload.reload_phrasal_urls import urlpatterns as reload_phrasal_urls
+from .reload.reload_sentence_urls import urlpatterns as reload_sentence_urls
+from .reload.views_urls import urlpatterns as reload_views_urls
+
+from .load_topic.views_urls import urlpatterns as load_topic_views_urls
 
 
-
-urlpatterns = [
-]
+urlpatterns = [] + \
+    review_phrasal_views_urls + \
+    review__review_phrasal_views_urls + \
+    review_views_urls + \
+    review_views_urls + \
+    review__views_urls + \
+    reload_phrasal_urls + \
+    reload_sentence_urls + \
+    reload_views_urls + \
+    load_topic_views_urls
--- englishpy/englishpy/urls.py
+++ englishpy/englishpy/urls.py
@@ -8,5 +8,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('english_py.practise.urls')),
+    url(r'^', include('english_py.practise.urls.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
