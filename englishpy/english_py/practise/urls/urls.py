30291b1ea2bf35ccfc7812b21efe2ce69fb62965
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-13 09:52:43 -0500

    split reload phrasal urls file

--- englishpy/english_py/practise/urls/urls.py
+++ englishpy/english_py/practise/urls/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url, include
 
-from ..review.review_phrasal_views import learning_phrasal, next_phrasal, get_sentence_phrasal_number, \
-    get_sentence_phrasal, learned_phrasal
 from ..reload.reload_phrasal import reload_phrasal
 from ..review.review_views import review
 from ..reload.reload_sentence import reload_sentence
@@ -109,36 +107,6 @@ urlpatterns = [
         regex=r'^review$',
         view=review,
         name='review'
-    ),
-
-
-
-
-
-    url(
-        regex=r'^learning_phrasal$',
-        view=learning_phrasal,
-        name='learning_phrasal'
-    ),
-    url(
-        regex=r'^next_phrasal$',
-        view=next_phrasal,
-        name='next_phrasal'
-    ),
-    url(
-        regex=r'^get_sentence_phrasal_number/([\w ]+)$',
-        view=get_sentence_phrasal_number,
-        name='get_sentence_phrasal_number'
-    ),
-    url(
-        regex=r'^get_sentence_phrasal/([\w ]+)$',
-        view=get_sentence_phrasal,
-        name='get_sentence'
-    ),
-    url(
-        regex=r'^learned_phrasal/([\w ]+)$',
-        view=learned_phrasal,
-        name='learned_phrasal'
     ),
 
 ]
