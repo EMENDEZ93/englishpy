5c16ae0a0308bf2af6f5f86e881c8f76d72d98c6
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-13 10:00:29 -0500

    setting revire phrasal views urls file

--- englishpy/english_py/practise/urls/__init__.py
+++ englishpy/english_py/practise/urls/__init__.py
@@ -1,4 +1,3 @@
 from .urls import urlpatterns
 from .review.phrasal_verb.review_phrasal_views_urls import urlpatterns
-
-from .reload.reload_phrasal_urls import urlpatterns
+from .review.review_phrasal_views_urls import urlpatterns
