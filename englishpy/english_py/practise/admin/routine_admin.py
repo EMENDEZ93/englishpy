8386a53221e18d820c36f42c81b262e2d9a89375
Edwin Mendez <emendez@gravitalsoftware.com>
2018-07-13 20:33:53 -0500

    new template

--- englishpy/english_py/practise/admin/routine_admin.py
+++ englishpy/english_py/practise/admin/routine_admin.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from ..models.routine.routine import Routine
-
+#from ..models.routine.routine import Routine
+from ..models import Routine
 
 class RoutineAdmin(admin.ModelAdmin):
     list_display = ('name',)
